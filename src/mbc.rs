89fce7134ddc3f78dd1b39b63b02c21b21f632b3
Michael Jung <MichaelWJung@users.noreply.github.com>
2017-11-25 17:48:27 +0100

    Implement Memory Bank Controller Mbc5

--- src/mbc.rs
+++ src/mbc.rs
@@ -6,6 +6,7 @@ pub fn create_mbc(rom: BlockMemory) -> Box<Memory> {
     match mbc_type {
         0x0 => Box::new(rom),
         0x1 | 0x2 | 0x3 => Box::new(Mbc1::new(rom)),
+        0x19 ... 0x1E => Box::new(Mbc5::new(rom)),
         _ => panic!("Unsupported Memory Bank Controller {:#X}", mbc_type),
     }
 }
@@ -112,4 +113,85 @@ impl Memory for Mbc1 {
 enum RomRamMode {
     RomBankingMode,
     RamBankingMode,
+}
+
+struct Mbc5 {
+    rom: BlockMemory,
+    ram: BlockMemory,
+    has_ram: bool,
+    current_rom_bank: u16,
+    current_ram_bank: u8,
+    ram_enabled: bool,
+    lower_bits: u8,
+    upper_bits: u8,
+}
+
+impl Mbc5 {
+    fn new(rom: BlockMemory) -> Mbc5 {
+        let mbc_type = rom.read_byte(0x147);
+        let has_ram = mbc_type == 0x1A || mbc_type == 0x1B ||
+                      mbc_type == 0x1D || mbc_type == 0x1E;
+        Mbc5 {
+            rom,
+            ram: BlockMemory::new(0x20000),
+            has_ram,
+            current_rom_bank: 1,
+            current_ram_bank: 0,
+            ram_enabled: false,
+            lower_bits: 1,
+            upper_bits: 0,
+        }
+    }
+
+    fn update_rom_bank(&mut self) {
+        self.current_rom_bank = ((self.upper_bits as u16) << 8) + self.lower_bits as u16;
+    }
+}
+
+impl Memory for Mbc5 {
+    fn read_byte(&self, address: u16) -> u8 {
+        match address {
+            0x0000 ... 0x3FFF => self.rom.read_byte(address),
+            0x4000 ... 0x7FFF => {
+                let address = (address & 0x3FFF) + self.current_rom_bank as u16 * 0x4000;
+                self.rom.read_byte(address)
+            }
+            0xA000 ... 0xBFFF => {
+                if self.ram_enabled {
+                    let address = (address & 0x1FFF) + self.current_ram_bank as u16 * 0x2000;
+                    self.ram.read_byte(address)
+                } else {
+                    0xFF
+                }
+            }
+            _ => panic!("Mbc5 cannot handle read from address {:#X}.", address)
+        }
+    }
+
+    fn write_byte(&mut self, address: u16, value: u8) {
+        match address {
+            0x0000 ... 0x1FFF => self.ram_enabled = self.has_ram && (value & 0xF == 0xA),
+            0x2000 ... 0x2FFF => {
+                self.lower_bits = value;
+                self.update_rom_bank();
+            }
+            0x3000 ... 0x3FFF => {
+                self.upper_bits = value;
+                self.update_rom_bank();
+            }
+            0x4000 ... 0x5FFF => {
+                if self.has_ram {
+                    self.current_ram_bank = value & 0xF;
+                }
+            }
+            0xA000 ... 0xBFFF => {
+                if self.ram_enabled {
+                    let address = (address & 0x1FFF) + self.current_ram_bank as u16 * 0x2000;
+                    self.ram.write_byte(address, value);
+                }
+            }
+            //_ => panic!("Mbc5 cannot handle write to address {:#X}.", address)
+            _ => (),
+        }
+    }
 }
